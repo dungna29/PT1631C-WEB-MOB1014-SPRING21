034a6eba484b4debb8d17b054557a27ec76745d6
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-08 11:02:43 +0700

    dungna

---
+++ PT1631CWEB_MOB1014_SPRING2021/src/BAI1_LAMQUENJAVA/B1_ChuongTrinhDauTien.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI1_LAMQUENJAVA;
+
+/**
+ *
+ * @author Dungna29
+ */
+public class B1_ChuongTrinhDauTien {
+    // 2 Cách để gọi hàm main    
+    //Cách 1: Ctrl + Space sau đó gõ main
+    //Cách 2: psvm + tab
+    
+    /*
+    Chạy chương trình: Shift + F6
+    */
+    public static void main(String[] args) {
+        // Xuất ra màn hình sout + tab
+        System.out.println("Chào mừng các bạn đến với JAVA1");
+             
+    }
+}
