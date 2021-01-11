a4b054014037a8dae10613b3fce32f41756270c3
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-11 10:55:28 +0700

    1

---
+++ PT1631CWEB_MOB1014_SPRING2021/src/BAI1_LAMQUENJAVA/B1_ChuongTrinhDauTien.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI1_LAMQUENJAVA;
+
+/**
+ *
+ * @author Dungna29
+ */
+public class B1_ChuongTrinhDauTien {
+    // 2 Cách gọi hàm main
+    // C1. Ctrl + Space gõ main
+    // C2. psvm + tab
+    
+    // Shift + F6 = Chạy chương trình
+    
+    
+        //1. Ctrl + X: delete một dòng. (Hay Dùng)
+        //2. Phím Ctrl + Shift + Up/Down: duplicate một dòng.
+        //3. Phím Ctrl + R: Rename nhiều biến cùng 1 lúc. (Hay Dùng)
+        //4. Phím Ctrl + Shift + O: tìm 1 file bất kỳ trong Project đang mở.
+        //5. Ctrl + Q: trở về vị trí edit cuối cùng.
+        //6. Ctrl + Shift + Down / Up: Duplicate 1 dòng.
+        //7. Ctrl + 7 : Mở nhanh Navigator(kết hợp với phím tab).
+        //8. Ctrl +  / : Bật tắt comment (Hay Dùng)
+        //9. Ctl + B: Di chuyển đến chỗ khai báo function.
+        //10. Shift-F6: chạy file hiện tại (Hay Dùng)
+        //11. Shift-F11: xóa và build project chính
+        //12. Alt + Shift + F: Format lại code cho đàng hoàng (Hay Dùng)
+    public static void main(String[] args) {
+        //In ra màn hình sout + tab
+        System.out.println("Chào các bạn JAVA1");
+        
+    }
+}
